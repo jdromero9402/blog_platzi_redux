75e6460316cd0df3e87f794c68097410a3c9a3f7
jdromero9402 <jdromero9402@gmail.com>
2021-05-08 22:15:29 -0500

    se trae y se utiliza la info de la API

--- src/components/App.js
+++ src/components/App.js
@@ -11,42 +11,28 @@ class App extends Component {
 
   async componentDidMount() {
     const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
-    console.log(respuesta);
     this.setState({
-      usuarios: [
-        {
-          nombre: 'Jesus',
-          correo: 'jdrom@asd.com',
-          enlace: 'jd.com',
-        },
-        {
-          nombre: 'Platzi',
-          correo: 'platzi@platzi.com',
-          enlace: 'platzi.com',
-        }
-      ]
+      usuarios: respuesta.data
     })
-    console.log("hola");
   }
 
   ponerFilas = () => (
     this.state.usuarios.map((usuario) => (
-      <tr>
+      <tr key={ usuario.id }>
         <td>
-          { usuario.nombre }
+          { usuario.name }
         </td>
         <td>
-          { usuario.correo }
+          { usuario.email }
         </td>
         <td>
-          { usuario.enlace }
+          { usuario.website }
         </td>
       </tr>
     ))
   ) ;
 
   render(){
-    console.log(this.state.usuarios);
     return(
     <div className="margen">
     <table className="tabla">
@@ -63,8 +49,8 @@ class App extends Component {
           </th>
         </tr>
       </thead>
-      { this.ponerFilas() }
       <tbody>
+      { this.ponerFilas() }
       </tbody>
     </table>
     </div>);
