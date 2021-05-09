be20b88159fa7332409e0d05cbf1cedd18a999f9
jdromero9402 <jdromero9402@gmail.com>
2021-05-08 23:02:25 -0500

    se comienza la configuracion de redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App.js';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  {}, //Todos los reducers
+  {} // Estado inicial
+);
+
 
 ReactDOM.render(
     <App />,
